046b29e2818adbe0f7ffc422b071b70c0afd3843
aman4457 <amanswag2021@gmail.com>
2023-09-01 16:23:55 -0500

    added another platform

--- main.lua
+++ main.lua
@@ -6,6 +6,9 @@ function love.load()
     cam = camera()
     world = wf.newWorld(0, 1000)
     world:addCollisionClass('Floor')
+    world:addCollisionClass('PlatformB')
+    world:addCollisionClass('LWall')
+    world:addCollisionClass('RWall')
     world:addCollisionClass('LWall')
     world:addCollisionClass('RWall')
     player = {}
@@ -16,32 +19,42 @@ function love.load()
     ground = world:newRectangleCollider(0, 600, 800, 10)
     wallL = world:newRectangleCollider(-10, -1000, 10, 1600)
     wallR = world:newRectangleCollider(800, -1000, 10, 1600)
-    platformtop = world:newRectangleCollider(210, 230, 390, 5)
-    platformbottom = world:newRectangleCollider(210, 235, 390, 5)
-    platforml = world:newRectangleCollider(205, 230, 5, 10)
-    platformr = world:newRectangleCollider(600, 230, 5, 10)
+    platformtop0 = world:newRectangleCollider(210, 230, 390, 5)
+    platformbottom0 = world:newRectangleCollider(210, 235, 390, 5)
+    platforml0 = world:newRectangleCollider(205, 230, 5, 10)
+    platformr0 = world:newRectangleCollider(600, 230, 5, 10)
+    platformtop1 = world:newRectangleCollider(310, -55, 490, 5)
+    platformbottom1 = world:newRectangleCollider(310, -50, 490, 5)
+    platforml1 = world:newRectangleCollider(305, -55, 5, 10)
+    platformr1 = world:newRectangleCollider(800, -55, 5, 10)
     ground:setType('static')
     wallR:setType('static')
     wallL:setType('static')
-    platformtop:setType('static')
-    platformbottom:setType('static')
-    platforml:setType('static')
-    platformr:setType('static')
+    platformtop0:setType('static')
+    platformbottom0:setType('static')
+    platforml0:setType('static')
+    platformr0:setType('static')
+    platformtop1:setType('static')
+    platformbottom1:setType('static')
+    platforml1:setType('static')
+    platformr1:setType('static')
     ground:setCollisionClass('Floor')
     wallL:setCollisionClass('LWall')
     wallR:setCollisionClass('RWall')
-    platformr:setCollisionClass('LWall')
-    platforml:setCollisionClass('RWall')
-    platformtop:setCollisionClass('Floor')
+    platformr0:setCollisionClass('LWall')
+    platforml0:setCollisionClass('RWall')
+    platformtop0:setCollisionClass('Floor')
+    platformr1:setCollisionClass('LWall')
+    platformbottom0:setCollisionClass('PlatformB')
+    platforml1:setCollisionClass('RWall')
+    platformtop1:setCollisionClass('Floor')
+    platformbottom1:setCollisionClass('PlatformB')
     floor_detect = 0
     player.collider:setFixedRotation(true)
 
 end
 
 function love.update(dt)
-
-
-    world:update(dt)
     if player.collider:enter('Floor') then
         floor_detect = 1
     elseif player.collider:exit('Floor') then
@@ -56,6 +69,11 @@ function love.update(dt)
         Rwall_detect = 1
     elseif player.collider:exit('RWall') then
         Rwall_detect = 0
+    end
+    if player.collider:enter('PlatformB') then
+        player.collider:applyLinearImpulse(0, 1)
+    elseif player.collider:exit('PlatformB') then
+        player.collider:applyLinearImpulse(0, 1)
     end
     if floor_detect == 1 then
         playervelx = 0
@@ -140,6 +158,7 @@ function love.update(dt)
     if cam.y > 300 then
         cam.y = 300 
     end
+    world:update(dt)
 
 end
 
